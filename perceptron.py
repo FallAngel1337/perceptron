0421afe9c0d3e81a4fb05f05c2c108bb7a535ffc
FallAngel1337 <fallangel@protonmail.com>
2022-08-22 10:52:38 -0300

    Add perceptron linking

--- perceptron.py
+++ perceptron.py
@@ -1,5 +1,24 @@
-class Perceptron():      
-    def activate_function(inputs, weights, bias=3):
+class Perceptron():
+    def __init__(self, inputs, weights, bias=3):
+        self.inputs = inputs
+        self.weights = weights
+        self.bias = bias
+
+    def from_perceptrons(perceptrons, weights, bias=3):
+        inputs = []
+        weights = weights
+        bias = bias
+
+        for perceptron in perceptrons:
+            inputs.append(perceptron.activate_function())
+        
+        return Perceptron(inputs, weights, bias)
+
+    def activate_function(self):
+        inputs = self.inputs
+        weights = self.weights
+        bias = self.bias
+
         assert(len(inputs) > 0 and len(weights) > 0)
 
         if len(inputs) == 1:
